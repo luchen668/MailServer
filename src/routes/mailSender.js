1ea992a245981fa0bf24129f7ffd8f02cc762eb6
luchen669 <Arielgusmam605@gmil.com>
2023-05-16 11:52:30 -0400

    Servidor

---
+++ src/routes/mailSender.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const formData = require("form-data");
+const Mailgun =  require("mailgun.js");
+
+require("dotenv").config();
+
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY});
+
+router.post("/", (req, res)=>{
+    const {name, from, subject, message} = req.body;
+    const messageData = {
+        from: `${name} <${from}>`,
+        to: 'airsencecafam@gmail.com',
+        subject: subject,
+        text: message
+      };
+      
+      client.messages.create(process.env.MAILGUN_DOMAIN, messageData)
+       .then((msg) => {
+        res.send({
+            message: 'Correo enviado correctamente'
+        })
+         console.log(msg);
+       })
+       .catch((err) => {
+        res.send({
+            message: 'Error en el envio de Correo'
+        })
+         console.error(err);
+       });
+});
+module.exports=router;
+
+
+
+
